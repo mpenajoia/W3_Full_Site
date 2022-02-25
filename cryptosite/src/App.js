b5a321de3e4ab32780d1b019868a04fbf8880d71
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2022-02-25 13:37:24 -0800

    Switch changed to Routes and updated

--- cryptosite/src/App.js
+++ cryptosite/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 // used to set  up links to pages
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, Routes } from 'react-router-dom';
 // resources from ant design
 import { Layout, Typography, Space } from 'antd';
 // bring in Navbar comp DEPRECATED
 // import Navbar from './components/Navbar';
 // after making index.js in components folder the following is possible
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
-
-
 
 function App() {
   return (
@@ -19,23 +17,13 @@ function App() {
       <div className='main'>
         <Layout>
           <div className='routes'>
-            <Switch>
-              <Route exact path="/">
-                <Homepage/>
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges/>
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies/>
-              </Route>
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails/>
-              </Route>
-              <Route exact path="/news">
-                <News/>
-              </Route>
-            </Switch>
+            <Routes>
+              <Route exact path="/" element={<Homepage/>}/>
+              <Route exact path="/exchanges" element={<Exchanges/>}/>
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}/>
+              <Route exact path="/news" element={<News/>}/>
+            </Routes>
           </div>
         </Layout>
       </div>
